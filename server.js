41c98f063e6cb715b51fc0a98d11ae13ee7218d4
Ilia <ershovilya97@gmail.com>
2021-04-19 11:09:45 +0300

    feat: add minimal WS

--- server.js
+++ server.js
@@ -1,2 +1,15 @@
 // const express = require('express') // CommonJS module
 import express from 'express' // ESM - ES module
+
+const app = express();
+const router = express.Router();
+app.use(router);
+
+router.route('/')
+  .get((req, res) => {
+    res.send('Hello')
+  });
+
+app.listen(3000, () => {
+  console.log('Server started on 3000');
+});
