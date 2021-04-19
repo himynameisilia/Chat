c2d0f69e459be1f883d916af5ad6927c51e24b6e
Ilia <ershovilya97@gmail.com>
2021-04-19 11:47:53 +0300

    feat: Send HTML file

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 // const express = require('express') // CommonJS module
 import express from 'express' // ESM - ES module
+import path from 'path';
 
 const app = express();
+
 const router = express.Router();
 app.use(router);
 
+const absolutePath = path.resolve(); // абсолютный путь до текущей папки
+const staticDir = path.join(absolutePath, 'public');
+// console.log(statDir);
+
+app.use(express.static(staticDir));
+
 router.route('/')
   .get((req, res) => {
-    res.send('Hello')
+    res.sendFile(staticDir, 'index.html')
   });
 
 app.listen(3000, () => {
